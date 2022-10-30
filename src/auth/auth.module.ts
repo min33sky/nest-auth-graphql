954bf181851a4e0ee138d632717722ae5d2f2805
min33sky <min33sky@naver.com>
2022-10-31 06:18:46 +0900

    feat: signup api add

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+import { JwtService } from '@nestjs/jwt';
+
+@Module({
+  providers: [AuthResolver, AuthService, JwtService],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignUpInput } from './dto/signup-input';
+import { UpdateAuthInput } from './dto/update-auth.input';
+import * as bcrypt from 'bcryptjs';
+import { SignResponse } from './dto/sign-response';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async signUp({
+    email,
+    password,
+    username,
+  }: SignUpInput): Promise<SignResponse> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+      },
+    });
+
+    const { accessToken, refreshToken } = await this.createTokens(
+      newUser.id,
+      newUser.email,
+    );
+
+    await this.updateRefreshToken(newUser.id, refreshToken);
+
+    return { accessToken, refreshToken, user: newUser };
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthInput: UpdateAuthInput) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  async createTokens(userId: number, email: string) {
+    const accessToken = this.jwtService.sign(
+      {
+        userId,
+        email,
+      },
+      {
+        expiresIn: '10s',
+        secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET'),
+      },
+    );
+
+    const refreshToken = this.jwtService.sign(
+      {
+        userId,
+        email,
+        accessToken,
+      },
+      {
+        expiresIn: '7d',
+        secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
+      },
+    );
+
+    return { accessToken, refreshToken };
+  }
+
+  async updateRefreshToken(userId: number, refreshToken: string) {
+    const hashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        refreshToken: hashedRefreshToken,
+      },
+    });
+  }
+}
